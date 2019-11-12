c3dfa7092875ebec35e0e66ddfbbf4803e2f2808
marcos.oliveira <marcos.oliveira@fagoc.br>
2019-11-12 18:15:00 -0300

    css adicionado

--- public/app/session/login.php
+++ public/app/session/login.php
@@ -10,19 +10,21 @@ require_once('backend.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="app/css/style.css">
+    <link rel="stylesheet" href="/app/css/style.css">
     <title>Login</title>
 </head>
 
 <body>
     <main class="container">
+        <div class="wrapper">
         <form action="#" method="post">
+            <h3>Log-in</h3>
             <div>
-                <label for="email">E-mail</label>
+                <label for="email">E-mail</label><br>
                 <input type="email" name="email" id="email">
             </div>
             <div>
-                <label for="password">Password</label>
+                <label for="password">Password</label><br>
                 <input type="password" name="password" id="password">
             </div>
             <button>Enter</button>
@@ -33,6 +35,7 @@ require_once('backend.php');
                         <p><?= $error ?></p>
                     <?php endforeach?>
                 <?php endif ?>
+        </div>
         </div>
     </main>
 </body>
